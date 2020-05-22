b3f64917271a9e8c3d32ba94e11385bf90a478c0
Anatoly Stansler <anatoly@sourcerer.io>
2020-05-22 19:41:23 +0300

    feat: install

--- pgcon2020/1_install-packages.sh
+++ pgcon2020/1_install-packages.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+echo "TODO(Nikolay): To hide this output change \`\"code\": \"1_install-packages.sh\",\` to \`\"courseData\": \"1_install-packages.sh\",\` "
+
 echo "Installing required packages"
 
-#deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main
-#wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-#sudo apt-get update
+deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
 
-#apt-get install postgresql-12 postgresql-12-contrib
+apt-get install postgresql-12 postgresql-12-contrib
 
-#/usr/pgsql-12/bin/postgresql-12-setup initdb
+/usr/pgsql-12/bin/postgresql-12-setup initdb
 
-
-#echo "Pull docker images"
-#docker pull TAG
+echo "Pull docker images"
+# docker pull TAG
