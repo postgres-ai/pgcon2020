bf2bdaa7d431d3f59b8527ced625aab1086b4036
Nikolay Samokhvalov <github2@samokhvalov.com>
2020-05-22 11:16:50 -0700

    Update and rename 1_install-packages.sh to launch.sh

--- pgcon2020/launch.sh
+++ pgcon2020/launch.sh
@@ -4,4 +4,5 @@ echo "TODO: To hide this output change \`\"code\": \"1_install-packages.sh\",\` 
 
 echo "Run container (name: 'demo1')"
 
-sudo docker run -p 65432:5432 --name demo1 --detach  -it postgresai/pgcon2020
+sudo docker run -p 5432:5432 --name demo1 -it postgresai/pgcon2020
+sudo docker exec -it demo1 bash
