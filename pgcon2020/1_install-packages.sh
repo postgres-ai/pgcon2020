94f9d6f67f27551aa978c1f8caf3f372bcb9d295
Nikolay Samokhvalov <github2@samokhvalov.com>
2020-05-22 11:12:23 -0700

    Update 1_install-packages.sh

--- pgcon2020/1_install-packages.sh
+++ pgcon2020/1_install-packages.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-echo "TODO(Nikolay): To hide this output change \`\"code\": \"1_install-packages.sh\",\` to \`\"courseData\": \"1_install-packages.sh\",\` "
+echo "TODO: To hide this output change \`\"code\": \"1_install-packages.sh\",\` to \`\"courseData\": \"1_install-packages.sh\",\` "
 
 echo "Installing required packages"
 
-deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-
-apt-get install postgresql-12 postgresql-12-contrib
-
-# /usr/pgsql-12/bin/postgresql-12-setup initdb
-
-echo "Pull docker images"
-# docker pull TAG
+sudo docker run -p 65432:5432 --name demo1 --detach  -it postgresai/pgcon2020
