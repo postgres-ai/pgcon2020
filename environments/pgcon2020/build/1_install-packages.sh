9f6faeea69e082d9de88ffbb7abbb24640822c5a
Anatoly Stansler <anatoly@sourcerer.io>
2020-05-22 19:23:13 +0300

    feat: add scenario and custom env

---
+++ environments/pgcon2020/build/1_install-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Installing required packages"
+
+deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+
+apt-get install postgresql-12 postgresql-12-contrib
+
+#/usr/pgsql-12/bin/postgresql-12-setup initdb
+
+
+echo "Pull docker images"
+#docker pull TAG
