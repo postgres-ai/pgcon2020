c29a9fa1fa6abcb20ff611770bd956dbd668fa24
Nikolay Samokhvalov <github2@samokhvalov.com>
2020-05-22 11:12:51 -0700

    Update 1_install-packages.sh

--- pgcon2020/1_install-packages.sh
+++ pgcon2020/1_install-packages.sh
@@ -2,6 +2,6 @@
 
 echo "TODO: To hide this output change \`\"code\": \"1_install-packages.sh\",\` to \`\"courseData\": \"1_install-packages.sh\",\` "
 
-echo "Installing required packages"
+echo "Run container (name: 'demo1')"
 
 sudo docker run -p 65432:5432 --name demo1 --detach  -it postgresai/pgcon2020
