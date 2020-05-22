4cd6272a8ed75a2d48f9b14fc69bde9f661b2a36
Anatoly Stansler <anatoly@sourcerer.io>
2020-05-22 19:27:23 +0300

    fix: debug

--- environments/pgcon2020/build/1_install-packages.sh
+++ environments/pgcon2020/build/1_install-packages.sh
@@ -2,14 +2,14 @@
 
 echo "Installing required packages"
 
-deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
+#deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main
+#wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+#sudo apt-get update
 
-apt-get install postgresql-12 postgresql-12-contrib
+#apt-get install postgresql-12 postgresql-12-contrib
 
 #/usr/pgsql-12/bin/postgresql-12-setup initdb
 
 
-echo "Pull docker images"
+#echo "Pull docker images"
 #docker pull TAG
