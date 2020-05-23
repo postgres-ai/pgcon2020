260624e74589db8e5cdd3d6939cb17d7ea0fce9a
Nikolay Samokhvalov <gitlab@samokhvalov.com>
2020-05-23 21:19:58 +0000

    Update pgcon2020/launch.sh, pgcon2020/index.json files

--- pgcon2020/launch.sh
+++ pgcon2020/launch.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-echo "TODO: To hide this output change \`\"code\": \"1_install-packages.sh\",\` to \`\"courseData\": \"1_install-packages.sh\",\` "
-
-echo "Run container (name: 'demo1')"
-
 sudo docker run -p 5432:5432 --name demo1 --detach -it postgresai/pgcon2020
 sudo docker exec -it demo1 bash
