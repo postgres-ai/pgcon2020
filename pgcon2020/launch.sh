8bd85e10ed4556c6e1ba6526e3ce543e1566c465
Nikolay Samokhvalov <gitlab@samokhvalov.com>
2020-05-24 03:32:42 +0000

    Update pgcon2020/launch.sh

--- pgcon2020/launch.sh
+++ pgcon2020/launch.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+echo "Preparing your workspace, please wait (usually it takes ~1 minute)..."
+
 sudo docker run -p 5432:5432 --name demo1 --detach -it postgresai/pgcon2020
 sudo docker exec -it demo1 bash
+
+echo "Your workspace is ready, please follow the instructions!"
